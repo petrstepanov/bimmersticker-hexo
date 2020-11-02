5b85d9d4c56b761a48a211da29ef36ceabf3fbab
petrstepanov <stepanovps@gmail.com>
2020-11-02 15:41:50 -0500

    Revert "Defer Snipcart CSS"
    This reverts commit 8b05584c279ab996812d3850bb2b3ab90651fc84.

--- themes/blueberry/gulpfile.js
+++ themes/blueberry/gulpfile.js
@@ -20,11 +20,6 @@ var paths = {
 		srcWatch: './development/sass/**/*.scss',
 		dest: './source/css'
 	},
-	stylesCSS: {
-		src: ['./development/sass/snipcart/snipcart-local.css'],
-		srcWatch: './development/sass/snipcart/snipcart-local.css',
-		dest: './source/css'
-	},
 	scripts: {
 		src: ['./development/js/app.js'],  // Only entry point for browserify
 		srcWatch: './development/js/**/*.js',
@@ -71,11 +66,6 @@ function styles() {
 		.pipe(gulp.dest(paths.styles.dest));
 }
 
-function stylesCSS() {
-	return gulp.src(paths.stylesCSS.src)
-		.pipe(gulp.dest(paths.stylesCSS.dest));
-}
-
 function stylesDev() {
 	return gulp.src(paths.styles.src)
 		.pipe(sourcemaps.init())
@@ -84,6 +74,7 @@ function stylesDev() {
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest(paths.styles.dest));
 }
+
 
 // Scripts Task
 
@@ -123,14 +114,13 @@ function scriptsDev() {
 function watch() {
 	gulp.watch(paths.scripts.srcWatch, scriptsDev);
 	gulp.watch(paths.styles.srcWatch, stylesDev);
-	gulp.watch(paths.stylesCSS.srcWatch, stylesCSS);
 }
 
 
 // Build
 
-var development = gulp.series(clean, copyIcons, gulp.parallel(stylesDev, stylesCSS, scriptsDev), watch);
-var production = gulp.series(clean, copyIcons, gulp.parallel(styles, stylesCSS, scripts));
+var development = gulp.series(clean, copyIcons, gulp.parallel(stylesDev, scriptsDev), watch);
+var production = gulp.series(clean, copyIcons, gulp.parallel(styles, scripts));
 
 
 // Exports
