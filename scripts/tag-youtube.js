a41407eecb86e3510e688d9ac9a3b4bef214165f
Petr Stepanov <stepanovps@gmail.com>
2023-09-24 19:16:14 -0700

    Switched to custom YouTube tag

---
+++ scripts/tag-youtube.js
@@ -0,0 +1,13 @@
+
+// Embed 100% width youtube video
+// https://stackoverflow.com/questions/52393814/css-100-width-of-youtube-embed-video
+
+const youtubeFn = function(args, content) {
+  var url = args[0];
+  content =  '<div class="videoWrapper">';
+  content += '<iframe width="320" height="180" src="https://www.youtube.com/embed/' + url + '" frameborder="0" allowfullscreen></iframe>';
+  content += '</div>'
+  return content;
+};
+
+hexo.extend.tag.register('youtube', youtubeFn);
