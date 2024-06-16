5a3e6d76fb8135fa8e3057b5d26a31171da025c2
Petr Stepanov <stepanovps@gmail.com>
2024-06-15 22:22:06 -0700

    GCR date format fix

--- themes/blueberry/development/js/modules/gcr.js
+++ themes/blueberry/development/js/modules/gcr.js
@@ -13,10 +13,17 @@ var GCR = function(){
         var today = new Date();
         var deliveryDate = new Date();
         deliveryDate.setDate(today.getDate() + deliveryDays);
+
+        // for Google 'deliveryDate' should have 4 digits for year,
+        // 2 digits for day and 2 digits for month ONLY: YYYY-MM-DD
+
         var year = 1900+deliveryDate.getYear();
+        var yearString = ""+year;
         var month = deliveryDate.getMonth() + 1;
+        var monthString = (month < 10) ? "0"+month : month;
         var day = deliveryDate.getDate();
-        var deliveryDateString = "" + year + "-" + month + "-" + day;
+        var dayString = (day < 10) ? "0"+day : day;
+        var deliveryDateString = yearString + "-" + monthString + "-" + dayString;
         // Populate success template with JSON
         var json = {
             'merchantID': 143612887,
